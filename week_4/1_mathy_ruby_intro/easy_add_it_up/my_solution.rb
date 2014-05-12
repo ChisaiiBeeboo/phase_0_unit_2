d959452f3d605e7d301c7c15c191485a55cefe4d
chisaiibeeboo <your_email@example.com>
2014-05-12 16:32:57 -0700

    pseudo code for add and sentence_maker

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -1,8 +1,8 @@
 # U2.W4: Add it up!
 
-# Complete each step below according to the challenge directions and 
+# Complete each step below according to the challenge directions and
 # include it in this file. Also make sure everything that isn't code
-# is commented in the file.  
+# is commented in the file.
 
 # I worked on this challenge [by myself, with: ].
 
@@ -12,6 +12,17 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+#### total ####
+
+# Take an array as input
+# Should output the sum of the elements in the array
+# Define a method total that iterates through the elements of the array and returns the sum
+
+#### sentence_maker ####
+
+# Take an array of strings and numbers as input
+# Should output a formal sentence string
+# Define a method sentence_maker that takes the first element of the array and call capitalize on it, then append a '.' at the end of the array, and calling the join method (' ') on the array
 
 # 2. Initial Solution
 
@@ -22,4 +33,4 @@
 
 
 
-# 4. Reflection 
+# 4. Reflection
