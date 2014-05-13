e219beb0ba1ffbaa6d657e1e7ec5bb9d81402fb8
chisaiibeeboo <your_email@example.com>
2014-05-12 23:25:32 -0700

    pseudo median

--- week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
+++ week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
@@ -12,10 +12,11 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+# Array is the input
+# Output should return the median of the array
+# Method median should take an array and check the first element to verify that it is a string, if true then sort the array alphabetically and return the middle-postion element of the array. Else if the first element of the array is not a string then return the middle-position element of the array.
 
 # 2. Initial Solution
-
-
 
 
 # 3. Refactored Solution
