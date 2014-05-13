c248d92824186285ecf109768eb573bc04f094ea
chisaiibeeboo <your_email@example.com>
2014-05-12 23:38:21 -0700

    finished median

--- week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
+++ week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
@@ -17,10 +17,18 @@
 # Method median should take an array and check the first element to verify that it is a string, if true then sort the array alphabetically and return the middle-postion element of the array. Else if the first element of the array is not a string then return the middle-position element of the array.
 
 # 2. Initial Solution
-
+def median(array)
+	if array[0].is_a?(String)
+		array.sort[array.length/2]
+    else
+		sorted = array.sort
+		((sorted[(sorted.length-1)/2]) + (sorted[sorted.length/2]))/2.0
+	end
+end
 
 # 3. Refactored Solution
 
 
 
-# 4. Reflection 
+# 4. Reflection
+#### Median challenge was pretty simple, it was a matter of accessing the middle-position of any given array. My ruby skills were a little rusty, I forgot to end my if statement and spent a good amount of time debugging, lesson learned. I reference ruby-doc.org when needed. Two simple challenges to start the week, feeling pretty confident.  ####
