c63c624778d635be85721bb19ba9e6197aec7167
Kate's Macbook Air <macbookair@Kate-MacBook-Air.local>
2014-05-15 14:10:58 -0700

    Finished Creative (paired Challenge)

--- week_4/2_creative/make_a_homework_cheater/my_solution.rb
+++ week_4/2_creative/make_a_homework_cheater/my_solution.rb
@@ -51,11 +51,7 @@ essay_writer(title, topic, date, thesis, teacher, partner, antagonist)
 
 # 4. Refactored Solution
 
-Abu = {
-    :power => "quick and nimble"
-}
-
-def essay_writer(title, topic, date, thesis, teacher, partner, antagonist, sidekick)
+def essay_writer(title, topic, date, thesis, teacher, partner, antagonist, sidekick, opt=nil)
     puts "\n" + title
     puts topic
     puts date + " Teacher: " + teacher
@@ -66,15 +62,20 @@ end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
-essay_writer("My favorite Movie", "Aladdin", "May 13, 2014", "Should Aladdin be considered the best movie of all-time?", "Mr. Philips", "Jasmine", "Jafar", "Abu")
+essay_writer("My favorite Movie", "Aladdin", "May 13, 2014", "Should Aladdin be considered the best movie of all-time?", "Mr. Philips", "Jasmine", "Jafar", "Abu", Abu = {:power => "quick and nimble"})
 
-#=>
-#My favorite Movie
-#Aladdin
-#May 13, 2014 Teacher: Mr. Philips
+# =>
+# My favorite Movie
+# Aladdin
+# May 13, 2014 Teacher: Mr. Philips
 #
-#Should Aladdin be considered the best movie of all-time? Aladdin combines great music, and all the magical aspects you would expect from a Disney movie. Aladdin and Jasmine make the perfect couple. You have Jafar as the perfect antagonist to Aladdins' protagonist and his sidekick Abu who is quick and nimble.
+# Should Aladdin be considered the best movie of all-time? Aladdin combines great music, and all the magical aspects you would expect from a Disney movie. Aladdin and Jasmine make the perfect couple. You have Jafar as the perfect antagonist to Aladdins' protagonist and his sidekick Abu who is quick and nimble.
 
 
 # 5. Reflection
+
+#### The essay_writer challenge was enjoyable, it allowed us to practice creating variables, objects and implementing string concatenation in Ruby. We were also able to utilize Ruby new line and tab characters to format the essay. Going from JavaScript mode to Ruby this week we struggled a bit when trying to access our objects key value pair for the bonus challenge. In JavaScript you can access an objects property through dot-notation 'Abu.power' or brackets 'Abu[power]'; in Ruby the correct syntax to access Abu's symbol would be Abu[:power]. I'm still a little rusty with Ruby syntax but feeling confident as far as understanding the concepts. ####
+
+
+
 
