f2b3f39dc207929cec7348226c9c2b408cb6071f
chisaiibeeboo <your_email@example.com>
2014-05-19 09:16:18 -0700

    Completed Guessing Game

--- week_5/3_guessing_game_solo_challenge/my_solution.rb
+++ week_5/3_guessing_game_solo_challenge/my_solution.rb
@@ -1,14 +1,18 @@
 # U2.W5: Build a simple guessing game SOLO CHALLENGE
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
-
+# Input: Input should take integer
+# Output: Should output low, high or correct.
+# Steps: Define class GuessingGame
+# GuessingGame should have 3 methods initialize, guess and solved?
+# Initialize should instantiate answer
+# Guess should take one argument guess and instantiate it. Return :high if guess is high than answer
+# return :low if lower than answer and :correct if guess is equal to answer
+# Solved? should return true or false based on whether guess was correct or not
 
 # 3. Initial Solution
 
@@ -38,16 +42,28 @@ end
 
 # 4. Refactored Solution
 
-
-
-
-
-
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+# game = GuessingGame.new(8)
 
+# game.solved?   # => false
 
+# game.guess(5)  # => :low
+# game.guess(20) # => :high
+# game.solved?   # => false
 
-
+# game.guess(10) # => :correct
+# game.solved?   # => true
 
 # 5. Reflection 
+# I used if/elsif statement to build out guess. I'm getting a little 
+# more comfortable with when to reference instantiated objects. First
+# time I've used Ruby symbols. I did hit a wall when I couldn't get my
+# solved? test to pass. I had added an if in front of my ternary operator
+# which threw everything off. 
+
+
+
+
+
+
